38fbba1432f50883770f54fd191155c2cb762f75
Emile NIKEL <enikel@e4r15p11.wethinkcode.co.za>
2018-10-06 11:21:02 +0200

    small typo fix

--- resource/setup.php
+++ resource/setup.php
@@ -17,7 +17,7 @@ $sql = "CREATE TABLE users (
 	user_id int(11) not null AUTO_INCREMENT PRIMARY KEY,
 	user_name varchar(256) not null,
 	user_email varchar(256) not null,
-	user_pwd varchar(256) not null	
+	user_pwd varchar(256) not null
 );";
 if ($conn->query($sql) === TRUE)
 	echo "Table create success";
