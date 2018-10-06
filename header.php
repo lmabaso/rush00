f06cc262fb2c903fbb4ef1b3acda8ece16136594
liberty <liberty.mabaso@yahoo.com>
2018-10-06 11:14:15 +0200

    auto commit

--- header.php
+++ header.php
@@ -1,28 +1,40 @@
 <?php
-
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
+
+echo '<!DOCTYPE html>
+	<html>
+	<head>
 	<title>ft_minishop</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+	</head>
+	<body>
 	<header>
 		<nav>
 			<div class="main-wrepper">
 				<ul>
-					<li><a href="index.php">Home</a></li>
+					<li><a href="index.php">Home </a></li>
 				</ul>
-				<div class="nav-login">
-					<form action="resource/login.php" method="POST">
-						<input type="text" name="username" placeholder="Username/e-mail">
-						<input type="password" name="pwd" placeholder="password">
-						<button name="submit">Login</button>
-					</form>
-					<a href="signup.php">Sign up</a>
-				</div>
-			</div>
-		</nav>
-	</header>
+				<ul>
+					<li><a href="shop.php">Shop</a></li>
+				</ul>
+				<div class="nav-login">';
+if (isset($_SESSION['user_name']))
+{
+	echo '<form action="resource/logout.php" method="POST">
+			<button name="submit">Logout</button>
+			</form>';
+}
+else
+{
+	echo '<form action="resource/login.php" method="POST">
+			<input type="text" name="username" placeholder="Username/e-mail">
+			<input type="password" name="pwd" placeholder="password">
+			<button name="submit">Login</button>
+			</form>';
+}
+echo '<a href="signup.php">Sign up</a>
+</div>
+</div>
+</nav>
+</header>';
+?>
