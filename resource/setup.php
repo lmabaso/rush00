0b5027684c7d9aeabfa3ba30f4ff5fc32ebbd6f2
liberty <liberty.mabaso@yahoo.com>
2018-10-06 11:14:54 +0200

    auto commit

--- resource/setup.php
+++ resource/setup.php
@@ -2,6 +2,7 @@
 $dbServicename = "localhost";
 $dbUsername = "root";
 $dbPassword = "zxcvb12345";
+$dbName = "register";
 
 $conn = mysqli_connect($dbServicename, $dbUsername, $dbPassword);
 $sql = "CREATE DATABASE register";
@@ -13,17 +14,20 @@ else
 	mysqli_close($conn);
 	return ;
 }
+mysqli_close($conn);
+
+$conn = mysqli_connect($dbServicename, $dbUsername, $dbPassword, $dbName);
 $sql = "CREATE TABLE users (
 	user_id int(11) not null AUTO_INCREMENT PRIMARY KEY,
 	user_name varchar(256) not null,
 	user_email varchar(256) not null,
 	user_pwd varchar(256) not null	
 );";
-if ($conn->query($sql) === TRUE)
+if (mysqli_query($conn, $sql))
 	echo "Table create success";
 else
 {
-	echo "Table create failure";
+	echo "Table create failure" . $conn->error;
 	mysqli_close($conn);
 	return ;
 }
