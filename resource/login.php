12ae650369f916fe9fbb3cbec561e81407dfeec0
Liberty <liberty.mabaso@yahoo.com>
2018-10-07 06:54:41 +0200

    auto commit

--- resource/login.php
+++ resource/login.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit']))
 	}
 	else 
 	{
-		$sql = "SELECT * FROM users";
+		$sql = "SELECT * FROM users WHERE user_name='$username' OR user_email='$username'";
 		$result = mysqli_query($conn, $sql);
 		$resultCheck = mysqli_num_rows($result);
 	
@@ -30,35 +30,8 @@ if (isset($_POST['submit']))
 		{
 			if ($row = mysqli_fetch_assoc($result))
 			{
-<<<<<<< HEAD
-<<<<<<< HEAD
-				$hashedPasswordCheck = hash('whirlpool', salt.$password);
-				var_dump($hashedPasswordCheck);
-				var_dump($row['user_pwd']);
-				// if ($hashedPasswordCheck === $row['user_pwd'])
-				// {
-				// 	$_SESSION['u_id'] = $row['user_id'];
-				// 	$_SESSION['u_name'] = $row['user_name'];
-				// 	$_SESSION['u_email'] = $row['user_email'];
-				// 	header("Location: ../index.php?login=success");
-				// 	exit();
-				// }
-				// else
-				// {
-				// 	header("Location: ../index.php?login=error2");
-				// 	exit();
-				// }
-=======
-=======
->>>>>>> 780cf0336e0f8f2cf898e0a1bdac15ea39db1bac
-				$hashedPasswordCheck = password_verify($password, $row['user_pwd']);
-				//var_dump($hashedPasswordCheck);
-				if ($hashedPasswordCheck == false)
-				{
-					header("Location: ../index.php?login=".$row['user_pwd']);
-					exit();
-				}
-				else if ($hashedPasswordCheck === true)
+				
+				if (password_verify($password, $row['user_pwd']))
 				{
 					$_SESSION['u_id'] = $row['user_id'];
 					$_SESSION['u_name'] = $row['user_name'];
@@ -66,10 +39,11 @@ if (isset($_POST['submit']))
 					header("Location: ../index.php?login=success");
 					exit();
 				}
-<<<<<<< HEAD
->>>>>>> 780cf0336e0f8f2cf898e0a1bdac15ea39db1bac
-=======
->>>>>>> 780cf0336e0f8f2cf898e0a1bdac15ea39db1bac
+				else
+				{
+					header("Location: ../index.php?login=error2");
+					exit();
+				}
 			}
 		}
 	}
